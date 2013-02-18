963422e90d166bbe5ec2fd043cd0bfa8f299c819
Josue Basurto <josuebasurto@gmail.com>
2013-02-18 11:41:45 -0800

    Se agrega configuracion de Phonegap y contenido

---
+++ Android App/assets/www/my.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    var requestcalendar = "https://www.googleapis.com/calendar/v3/calendars/0538djil1mtduscku37mvu3nos%40group.calendar.google.com/events?key=AIzaSyDS5zTvWBetR7ngAYtdcoT02HtUwyACaC8";
+    //alert('Inicia codigo JS');
+});
