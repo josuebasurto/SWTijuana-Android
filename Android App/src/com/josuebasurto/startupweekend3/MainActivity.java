51dc5abb69ac80bb3c7900d7a4f17511fa1b63a0
Josue Basurto <josuebasurto@gmail.com>
2013-02-26 15:50:58 -0800

    Se quita Splash y Toasts

--- Android App/src/com/josuebasurto/startupweekend3/MainActivity.java
+++ Android App/src/com/josuebasurto/startupweekend3/MainActivity.java
@@ -2,14 +2,21 @@ package com.josuebasurto.startupweekend3;
 
 import android.os.Bundle;
 import org.apache.cordova.*;
+import com.josuebasurto.fw.constantes;
+import com.josuebasurto.fw.notificaciones;
 
 public class MainActivity extends DroidGap {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		super.setIntegerProperty("splashscreen", R.drawable.splash); 
-		super.loadUrl("file:///android_asset/www/index.html", 3000);
+		//super.setIntegerProperty("splashscreen", R.drawable.splash);
+		try {
+			super.loadUrl(constantes.K_GetLocalUrl);
+			//notificaciones.DoToast("Iniciando...", getApplicationContext());			
+		} catch (Exception e) {
+			notificaciones.DoToast(e.getMessage(), getApplicationContext());
+		}
 	}
 	
 	@Override
